14618d09a2e82b82d8d4b6d21c455820ba5162d9
Alex Hamlin <alex@alexhamlin.co>
2025-02-23 12:25:14 -0800

    xt v0.19.4

--- Formula/xt.rb
+++ Formula/xt.rb
@@ -2,35 +2,35 @@
 
 class Xt < Formula
   desc "Translate between JSON, MessagePack, TOML, and YAML"
-  version "0.19.3"
+  version "0.19.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ahamlinman/xt/releases/download/v0.19.3/xt-aarch64-apple-darwin.tar.gz"
-      sha256 "686e7f6d2c348afa0436ef4d33d85d734c5e92f732f8ae9d00c409fefcdeed18"
+      url "https://github.com/ahamlinman/xt/releases/download/v0.19.4/xt-aarch64-apple-darwin.tar.gz"
+      sha256 "b7213e856695355d249484054d009ddf85baad8673f99489e77e7d230dcd2f8c"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/ahamlinman/xt/releases/download/v0.19.3/xt-x86_64-apple-darwin.tar.gz"
-      sha256 "838fc96c8ad915f48ee1f48279ae2b617b5cedd9b5af7a5d99ceba009e3e665e"
+      url "https://github.com/ahamlinman/xt/releases/download/v0.19.4/xt-x86_64-apple-darwin.tar.gz"
+      sha256 "653091a1f0327bc691db30af1b90503d8e74cea858312f564d060731aafbccbf"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ahamlinman/xt/releases/download/v0.19.3/xt-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "5b85b4da3953cb8c21dba70438879538f98ccdb056f3b2c12b536d64342a9def"
+      url "https://github.com/ahamlinman/xt/releases/download/v0.19.4/xt-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "8136e31c3ac60c556dcd2c38ce6c775a84c868c0f2238fdd9fee5a38933c9724"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ahamlinman/xt/releases/download/v0.19.3/xt-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "a054aac47e6bde10259b55ebb21c2fa4ad25b5467a7e8bef6459a1290eda7cc8"
+      url "https://github.com/ahamlinman/xt/releases/download/v0.19.4/xt-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "2d9b3e1ecf8f67e040d61aea89734c20ad8b72e6caff5b75eb6051784d612b3e"
     end
 
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/ahamlinman/xt/releases/download/v0.19.3/xt-armv7-unknown-linux-musleabihf.tar.gz"
-      sha256 "b7c4a221cf63f261210bb42b1140b7ba912da2a6d9b816815db41a74f218a6ef"
+      url "https://github.com/ahamlinman/xt/releases/download/v0.19.4/xt-armv7-unknown-linux-musleabihf.tar.gz"
+      sha256 "e7560b5812949d9ceadfedb5de27f0d5394799ef7ed175aee52b4b3e62935b91"
     end
   end
 
