115da60a845fba41bd5ee5464b89ff324071afec
Alex Hamlin <alex@alexhamlin.co>
2026-01-10 15:13:37 -0800

    xt v0.20.0

--- Formula/xt.rb
+++ Formula/xt.rb
@@ -2,35 +2,35 @@
 
 class Xt < Formula
   desc "Translate between JSON, MessagePack, TOML, and YAML"
-  version "0.19.5"
+  version "0.20.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/featherbread/xt/releases/download/v0.19.5/xt-aarch64-apple-darwin.tar.gz"
-      sha256 "d8004673b6f71c4ebe098fa85472a6c6babe6b7398824d229531bee39443d1b6"
+      url "https://github.com/featherbread/xt/releases/download/v0.20.0/xt-aarch64-apple-darwin.tar.gz"
+      sha256 "fa5de0eeeb09809e10d68d6273e2696d2047efb717975b7edce5c14a8166bac2"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/featherbread/xt/releases/download/v0.19.5/xt-x86_64-apple-darwin.tar.gz"
-      sha256 "1bc6c6f43a3a95c8ce538f64f9ee87cd579577c053479e68a216856ed60e1ac9"
+      url "https://github.com/featherbread/xt/releases/download/v0.20.0/xt-x86_64-apple-darwin.tar.gz"
+      sha256 "7346f929b10196bb50ce992d9dda7b103ea0f526c3b22e9e00da335de417d386"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/featherbread/xt/releases/download/v0.19.5/xt-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "f34feffc127baa1da9da4419244ba69308a8487ceefe4c0740b7121a9ba51a2f"
+      url "https://github.com/featherbread/xt/releases/download/v0.20.0/xt-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "0bb8b61869acbbc9d816062b6901f088c99fccfc12d3fc2a37ad405a825dc845"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/featherbread/xt/releases/download/v0.19.5/xt-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "fac690ee1071b48ef10ee7b0daec936afe9ea975f3019add790c509fe017ffa7"
+      url "https://github.com/featherbread/xt/releases/download/v0.20.0/xt-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "c662e3de6352f9042c6e434aea78cd3b16cbd18283504a46ffda0ab019ec0960"
     end
 
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/featherbread/xt/releases/download/v0.19.5/xt-armv7-unknown-linux-musleabihf.tar.gz"
-      sha256 "49198a126872f5d9c4e0f9642da279adb0ca08c69902e75ad162084f7a829412"
+      url "https://github.com/featherbread/xt/releases/download/v0.20.0/xt-armv7-unknown-linux-musleabihf.tar.gz"
+      sha256 "a66115efd09eac21d4740602bcff31bb2b5a33d3c100f5d905d0986922628f14"
     end
   end
 
